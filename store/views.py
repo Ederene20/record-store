4a6d7bd0b339567491533049fd3885580a29efb1
Ederene20 <atinhoruben@gmail.com>
2021-08-26 23:02:23 +0100

    update store view index to render the index.html file

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from django.template import loader
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Album, Artist, Contact, Booking
@@ -11,7 +12,8 @@ def index(request):
     # then format the request
     formatted_albums = ["<li>{}</li>".format(album.title) for album in albums]
     message = """<ul>{}</ul>""".format("\n".join(formatted_albums))
-    return HttpResponse(message)
+    template = loader.get_template('store/index.html')
+    return HttpResponse(template.render(request=request))
 
 
 def listing(request):
