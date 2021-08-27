576d941a018a6a06ab7bd2ed7dc7a2e412a6dacc
Ederene20 <atinhoruben@gmail.com>
2021-08-27 18:01:24 +0100

    update index views to render the index template

--- store/views.py
+++ store/views.py
@@ -10,10 +10,13 @@ def index(request):
     # request album
     albums = Album.objects.filter(available=True).order_by('-created_at')[:12]
     # then format the request
-    formatted_albums = ["<li>{}</li>".format(album.title) for album in albums]
-    message = """<ul>{}</ul>""".format("\n".join(formatted_albums))
+    # formatted_albums = ["<li>{}</li>".format(album.title) for album in albums]
+    # message = """<ul>{}</ul>""".format("\n".join(formatted_albums))
     template = loader.get_template('store/index.html')
-    return HttpResponse(template.render(request=request))
+    context = {
+        'albums': albums
+    }
+    return HttpResponse(template.render(context ,request=request))
 
 
 def listing(request):
