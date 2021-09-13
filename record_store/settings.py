83ecd843eb21e2ac68d09415ad0edd12cc5d7a3d
Ederene20 <atinhoruben@gmail.com>
2021-09-13 18:55:37 +0100

    add tests

--- record_store/settings.py
+++ record_store/settings.py
@@ -30,8 +30,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 
     # Extra places for collectstatic to find static files
 
-    prod_db = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(prod_db)
+    DATABASES = {'default': dj_database_url.config(conn_max_age=500)}
 
     DEBUG = False
 else:
